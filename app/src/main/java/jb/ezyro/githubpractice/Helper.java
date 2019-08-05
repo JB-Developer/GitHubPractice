2a506f34be6ff76a63c7a428a7dd0fa788ec335f
JB-Developer <jbdeveloper302@gmail.com>
2019-08-05 12:47:53 +0500

    Binding Done

---
+++ app/src/main/java/jb/ezyro/githubpractice/Helper.java
@@ -0,0 +1,26 @@
+package jb.ezyro.githubpractice;
+
+public class Helper {
+    private String f_name,l_name;
+
+    public Helper(String f_name, String l_name) {
+        this.f_name = f_name;
+        this.l_name = l_name;
+    }
+
+    public String getF_name() {
+        return f_name;
+    }
+
+    public void setF_name(String f_name) {
+        this.f_name = f_name;
+    }
+
+    public String getL_name() {
+        return l_name;
+    }
+
+    public void setL_name(String l_name) {
+        this.l_name = l_name;
+    }
+}
--- app/src/main/java/jb/ezyro/githubpractice/MainActivity.java
+++ app/src/main/java/jb/ezyro/githubpractice/MainActivity.java
@@ -1,13 +1,20 @@
 package jb.ezyro.githubpractice;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import jb.ezyro.githubpractice.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Helper user = new Helper("Test", "User");
+        binding.setUser(user);
+
     }
+
 }
